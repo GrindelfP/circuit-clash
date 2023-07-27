06b07aa8b18aaaecf51f10f161dc567c78368390
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-07-27 20:50:27 +0300

    Piece docs

--- src/main/java/to/grindelf/circuitclash/domain/Piece.java
+++ src/main/java/to/grindelf/circuitclash/domain/Piece.java
@@ -5,18 +5,46 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
+import static to.grindelf.circuitclash.domain.PieceColor.WHITE;
+import static to.grindelf.circuitclash.domain.PieceType.*;
+
+/**
+ * Representation of a chess piece.
+ */
 abstract class Piece {
 
+    /**
+     * Piece type.
+     */
     protected final PieceType type;
+    /**
+     * Piece color.
+     */
     protected final PieceColor color;
 
+    /**
+     * Initializes a piece with given type and color.
+     *
+     * @param type  is a type of piece.
+     * @param color is a color of piece.
+     */
     public Piece(PieceType type, PieceColor color) {
         this.type = type;
         this.color = color;
     }
 
+    /**
+     * Function returns a graphical representation of a piece.
+     *
+     * @return a file - graphical representation of a piece.
+     */
     public abstract File getImage();
 
+    /**
+     * Overrides equals method to compare two pieces.
+     *
+     * @return true if pieces are equal, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -28,6 +56,11 @@ abstract class Piece {
         return color == piece.color;
     }
 
+    /**
+     * Overrides hashCode method to compare two pieces.
+     *
+     * @return hash code of a piece.
+     */
     @Override
     public int hashCode() {
         int result = type != null ? type.hashCode() : 0;
@@ -37,51 +70,99 @@ abstract class Piece {
     }
 }
 
+/**
+ * Representation of a pawn.
+ */
 final class Pawn extends Piece {
 
+    /**
+     * File with image of white pawn.
+     */
     private final File whitePawnImage = new File("path/to/white/pawn/image");
+    /**
+     * File with image of black pawn.
+     */
     private final File blackPawnImage = new File("path/to/black/pawn/image");
 
+    /**
+     * Initializes a pawn with given color.
+     *
+     * @param color is a color of pawn.
+     */
     public Pawn(PieceColor color) {
-        super(PieceType.PAWN, color);
+        super(PAWN, color);
     }
 
+    /**
+     * Overrides toString method to return a graphical representation of a pawn.
+     *
+     * @return a string representation of a pawn.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public String toString() {
         String whitePawnSymbol = "♙";
         String blackPawnSymbol = "♟";
-        return this.color == PieceColor.WHITE ? whitePawnSymbol : blackPawnSymbol;
+        return this.color == WHITE ? whitePawnSymbol : blackPawnSymbol;
     }
 
+    /**
+     * Function returns a graphical representation of a pawn.
+     *
+     * @return a file - graphical representation of a pawn.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public File getImage() {
         throw new UnsupportedOperationException("Not implemented yet because of the lack of resource.");
-        // return this.color == PieceColor.WHITE ? whitePawnImage : blackPawnImage;
+        // return this.color == WHITE ? whitePawnImage : blackPawnImage;
     }
 }
 
+/**
+ * Representation of a rook.
+ */
 final class Rook extends Piece {
 
+    /**
+     * File with image of white rook.
+     */
     private final File whiteRookImage = new File("path/to/white/rook/image");
+    /**
+     * File with image of black rook.
+     */
     private final File blackRookImage = new File("path/to/black/rook/image");
 
+    /**
+     * Initializes a rook with given color.
+     *
+     * @param color is a color of rook.
+     */
     public Rook(PieceColor color) {
-        super(PieceType.ROOK, color);
+        super(ROOK, color);
     }
 
+    /**
+     * Overrides toString method to return a graphical representation of a rook.
+     *
+     * @return a string representation of a rook.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public String toString() {
         String whiteRookSymbol = "♖";
         String blackRookSymbol = "♜";
-        return this.color == PieceColor.WHITE ? whiteRookSymbol : blackRookSymbol;
+        return this.color == WHITE ? whiteRookSymbol : blackRookSymbol;
     }
 
+    /**
+     * Function returns a graphical representation of a rook.
+     *
+     * @return a file - graphical representation of a rook.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
@@ -91,110 +172,206 @@ final class Rook extends Piece {
     }
 }
 
+/**
+ * Representation of a knight.
+ */
 final class Knight extends Piece {
 
+    /**
+     * File with image of white knight.
+     */
     private final File whiteKnightImage = new File("path/to/white/knight/image");
+    /**
+     * File with image of black knight.
+     */
     private final File blackKnightImage = new File("path/to/black/knight/image");
 
+    /**
+     * Initializes a knight with given color.
+     *
+     * @param color is a color of knight.
+     */
     public Knight(PieceColor color) {
-        super(PieceType.KNIGHT, color);
+        super(KNIGHT, color);
     }
 
+    /**
+     * Overrides toString method to return a graphical representation of a knight.
+     *
+     * @return a string representation of a knight.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public String toString() {
         String whiteKnightSymbol = "♘";
         String blackKnightSymbol = "♞";
-        return this.color == PieceColor.WHITE ? whiteKnightSymbol : blackKnightSymbol;
+        return this.color == WHITE ? whiteKnightSymbol : blackKnightSymbol;
     }
 
+    /**
+     * Function returns a graphical representation of a knight.
+     *
+     * @return a file - graphical representation of a knight.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public File getImage() {
         throw new UnsupportedOperationException("Not implemented yet because of the lack of resource.");
-        // return this.color == PieceColor.WHITE ? whiteKnightImage : blackKnightImage;
+        // return this.color == WHITE ? whiteKnightImage : blackKnightImage;
     }
 }
 
+/**
+ * Representation of a bishop.
+ */
 final class Bishop extends Piece {
 
+    /**
+     * File with image of white bishop.
+     */
     private final File whiteBishopImage = new File("path/to/white/bishop/image");
+    /**
+     * File with image of black bishop.
+     */
     private final File blackBishopImage = new File("path/to/black/bishop/image");
 
+    /**
+     * Initializes a bishop with given color.
+     *
+     * @param color is a color of bishop.
+     */
     public Bishop(PieceColor color) {
-        super(PieceType.BISHOP, color);
+        super(BISHOP, color);
     }
 
+    /**
+     * Overrides toString method to return a graphical representation of a bishop.
+     *
+     * @return a string representation of a bishop.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public String toString() {
         String whiteBishopSymbol = "♗";
         String blackBishopSymbol = "♝";
-        return this.color == PieceColor.WHITE ? whiteBishopSymbol : blackBishopSymbol;
+        return this.color == WHITE ? whiteBishopSymbol : blackBishopSymbol;
     }
 
+    /**
+     * Function returns a graphical representation of a bishop.
+     *
+     * @return a file - graphical representation of a bishop.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public File getImage() {
         throw new UnsupportedOperationException("Not implemented yet because of the lack of resource.");
-        // return this.color == PieceColor.WHITE ? whiteBishopImage : blackBishopImage;
+        // return this.color == WHITE ? whiteBishopImage : blackBishopImage;
     }
 }
 
+/**
+ * Representation of a queen.
+ */
 final class Queen extends Piece {
 
+    /**
+     * File with image of white queen.
+     */
     private final File whiteQueenImage = new File("path/to/white/queen/image");
+    /**
+     * File with image of black queen.
+     */
     private final File blackQueenImage = new File("path/to/black/queen/image");
 
+    /**
+     * Initializes a queen with given color.
+     *
+     * @param color is a color of queen.
+     */
     public Queen(PieceColor color) {
-        super(PieceType.QUEEN, color);
+        super(QUEEN, color);
     }
 
+    /**
+     * Overrides toString method to return a graphical representation of a queen.
+     *
+     * @return a string representation of a queen.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public String toString() {
         String whiteQueenSymbol = "♕";
         String blackQueenSymbol = "♛";
-        return this.color == PieceColor.WHITE ? whiteQueenSymbol : blackQueenSymbol;
+        return this.color == WHITE ? whiteQueenSymbol : blackQueenSymbol;
     }
 
+    /**
+     * Function returns a graphical representation of a queen.
+     *
+     * @return a file - graphical representation of a queen.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public File getImage() {
         throw new UnsupportedOperationException("Not implemented yet because of the lack of resource.");
-        // return this.color == PieceColor.WHITE ? whiteQueenImage : blackQueenImage;
+        // return this.color == WHITE ? whiteQueenImage : blackQueenImage;
     }
 }
 
+/**
+ * Representation of a king.
+ */
 final class King extends Piece {
 
+    /**
+     * File with image of white king.
+     */
     private final File whiteKingImage = new File("path/to/white/king/image");
+    /**
+     * File with image of black king.
+     */
     private final File blackKingImage = new File("path/to/black/king/image");
 
+    /**
+     * Initializes a king with given color.
+     *
+     * @param color is a color of king.
+     */
     public King(PieceColor color) {
-        super(PieceType.KING, color);
+        super(KING, color);
     }
 
+    /**
+     * Overrides toString method to return a graphical representation of a king.
+     *
+     * @return a string representation of a king.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public String toString() {
         String whiteKingSymbol = "♔";
         String blackKingSymbol = "♚";
-        return this.color == PieceColor.WHITE ? whiteKingSymbol : blackKingSymbol;
+        return this.color == WHITE ? whiteKingSymbol : blackKingSymbol;
     }
 
+    /**
+     * Function returns a graphical representation of a king.
+     *
+     * @return a file - graphical representation of a king.
+     */
     @Override
     @NotNull
     @Contract(pure = true)
     public File getImage() {
         throw new UnsupportedOperationException("Not implemented yet because of the lack of resource.");
-        // return this.color == PieceColor.WHITE ? whiteKingImage : blackKingImage;
+        // return this.color == WHITE ? whiteKingImage : blackKingImage;
     }
 }
